572f8f798c9ebfb68f46c8bb75209bb810e44def
xsimple1010 <aderval758@gmail.com>
2024-03-12 20:35:31 -0300

    export EventPump

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub mod context;
 pub use sdl2::event::Event;
 pub use sdl2::event::WindowEvent;
 pub use sdl2::keyboard::Keycode;
+pub use sdl2::EventPump;
 
 pub use audios::{audio_sample_batch_callback, audio_sample_callback};
 pub use video::video_refresh_callback;
