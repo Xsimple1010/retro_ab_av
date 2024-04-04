0ae3596a1a4775318c1f37c43b4c103a7a17030d
xsimple1010 <aderval758@gmail.com>
2024-04-03 21:53:12 -0300

    add RetroAvEvents

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
-pub extern crate glutin;
-pub extern crate glutin_winit;
+extern crate glutin;
+extern crate glutin_winit;
 extern crate raw_window_handle;
 extern crate retro_ab;
 extern crate rodio;
-pub extern crate winit;
+extern crate winit;
 
 mod audios;
+pub mod context;
 pub mod retro_gl;
 pub mod video;
-
-pub mod context;
-
 pub use audios::{audio_sample_batch_callback, audio_sample_callback};
 pub use video::video_refresh_callback;
+
+pub use winit::{
+    event::{Event, KeyEvent, WindowEvent},
+    event_loop::{EventLoop, EventLoopBuilder},
+    keyboard::{Key, NamedKey},
+    platform::{pump_events::EventLoopExtPumpEvents, windows::EventLoopBuilderExtWindows},
+};
--- src/video.rs
+++ src/video.rs
@@ -16,9 +16,8 @@ use std::ffi::c_uint;
 use std::os::raw::c_void;
 use std::ptr::null;
 use std::sync::Arc;
-use winit::event_loop::{EventLoop, EventLoopBuilder, EventLoopWindowTarget};
+use winit::event_loop::{EventLoop, EventLoopWindowTarget};
 use winit::window::{Window, WindowBuilder};
-use winit::{dpi::PhysicalSize, platform::windows::EventLoopBuilderExtWindows};
 
 static mut RAW_TEX_POINTER: RawTextureData = RawTextureData {
     data: null(),
@@ -38,7 +37,7 @@ pub fn video_refresh_callback(data: *const c_void, width: c_uint, height: c_uint
 
 pub struct RetroVideo {
     pub state: Option<(PossiblyCurrentContext, Surface<WindowSurface>, Window)>,
-    pub las_window_size: PhysicalSize<u32>,
+    // pub las_window_size: PhysicalSize<u32>,
     pub window: Option<Window>,
     not_current_gl_context: Option<NotCurrentContext>,
     av_info: Arc<AvInfo>,
@@ -48,15 +47,74 @@ pub struct RetroVideo {
 }
 
 impl RetroVideo {
+    pub fn new(
+        av_info: Arc<AvInfo>,
+        event_loop: &EventLoop<()>,
+    ) -> Result<RetroVideo, Box<dyn Error>> {
+        // Only Windows requires the window to be present before creating the display.
+        // Other platforms don't really need one.
+        //
+        // XXX if you don't care about running on Android or so you can safely remove
+        // this condition and always pass the window builder.
+        let window_builder = cfg!(wgl_backend).then(|| {
+            WindowBuilder::new()
+                .with_title("Glutin triangle gradient example (press Escape to exit)")
+                .with_maximized(true)
+        });
+
+        // The template will match only the configurations supporting rendering
+        // to windows.
+        //
+        // XXX We force transparency only on macOS, given that EGL on X11 doesn't
+        // have it, but we still want to show window. The macOS situation is like
+        // that, because we can query only one config at a time on it, but all
+        // normal platforms will return multiple configs, so we can find the config
+        // with transparency ourselves inside the `reduce`.
+        let template = ConfigTemplateBuilder::new().with_alpha_size(8);
+
+        let display_builder = DisplayBuilder::new().with_window_builder(window_builder);
+
+        let (window, gl_config) = display_builder.build(&event_loop, template, gl_config_picker)?;
+
+        println!("Picked a config with {} samples", gl_config.num_samples());
+
+        let raw_window_handle = window.as_ref().map(|window| window.raw_window_handle());
+
+        // XXX The display could be obtained from any object created by it, so we can
+        // query it from the config.
+        let gl_display = gl_config.display();
+
+        let not_current_gl_context = get_gl_context(raw_window_handle, &gl_display, &gl_config);
+
+        Ok(RetroVideo {
+            av_info,
+            gl_config,
+            not_current_gl_context,
+            window,
+            gl_display,
+            render: None,
+            state: None,
+            // las_window_size: PhysicalSize {
+            //     width: 200,
+            //     height: 200,
+            // },
+        })
+    }
+
     pub fn draw_new_frame(&mut self) {
         unsafe {
-            if let Some(render) = &mut self.render {
-                render.draw_new_frame(
-                    &RAW_TEX_POINTER,
-                    &self.av_info.video.geometry,
-                    self.las_window_size.width as i32,
-                    self.las_window_size.height as i32,
-                );
+            if let Some((gl_context, gl_surface, win)) = &self.state {
+                let size = win.inner_size();
+                if let Some(render) = &mut self.render {
+                    render.draw_new_frame(
+                        &RAW_TEX_POINTER,
+                        &self.av_info.video.geometry,
+                        size.width as i32,
+                        size.height as i32,
+                    );
+                }
+
+                gl_surface.swap_buffers(gl_context).unwrap();
             }
         }
     }
@@ -70,6 +128,11 @@ impl RetroVideo {
                 WindowBuilder::new().with_title("Retro_ab_av (press Escape to exit)");
             glutin_winit::finalize_window(window_target, window_builder, &self.gl_config).unwrap()
         });
+
+        // window.set_min_inner_size(Some(PhysicalSize::new(
+        //     self.av_info.video.geometry.max_width.,
+        //     self.av_info.video.geometry.max_height,
+        // )));
 
         let attrs = window.build_surface_attributes(Default::default());
         let gl_surface = unsafe {
@@ -105,62 +168,4 @@ impl RetroVideo {
             .replace(gl_context.make_not_current().unwrap())
             .is_none());
     }
-}
-
-pub fn init(av_info: Arc<AvInfo>) -> Result<(RetroVideo, EventLoop<()>), Box<dyn Error>> {
-    let event_loop = EventLoopBuilder::new()
-        .with_any_thread(true)
-        .build()
-        .unwrap();
-    // Only Windows requires the window to be present before creating the display.
-    // Other platforms don't really need one.
-    //
-    // XXX if you don't care about running on Android or so you can safely remove
-    // this condition and always pass the window builder.
-    let window_builder = cfg!(wgl_backend).then(|| {
-        WindowBuilder::new()
-            .with_title("Glutin triangle gradient example (press Escape to exit)")
-            .with_maximized(true)
-    });
-
-    // The template will match only the configurations supporting rendering
-    // to windows.
-    //
-    // XXX We force transparency only on macOS, given that EGL on X11 doesn't
-    // have it, but we still want to show window. The macOS situation is like
-    // that, because we can query only one config at a time on it, but all
-    // normal platforms will return multiple configs, so we can find the config
-    // with transparency ourselves inside the `reduce`.
-    let template = ConfigTemplateBuilder::new().with_alpha_size(8);
-
-    let display_builder = DisplayBuilder::new().with_window_builder(window_builder);
-
-    let (window, gl_config) = display_builder.build(&event_loop, template, gl_config_picker)?;
-
-    println!("Picked a config with {} samples", gl_config.num_samples());
-
-    let raw_window_handle = window.as_ref().map(|window| window.raw_window_handle());
-
-    // XXX The display could be obtained from any object created by it, so we can
-    // query it from the config.
-    let gl_display = gl_config.display();
-
-    let not_current_gl_context = get_gl_context(raw_window_handle, &gl_display, &gl_config);
-
-    Ok((
-        RetroVideo {
-            av_info,
-            gl_config,
-            not_current_gl_context,
-            window,
-            gl_display,
-            render: None,
-            state: None,
-            las_window_size: PhysicalSize {
-                width: 200,
-                height: 200,
-            },
-        },
-        event_loop,
-    ))
 }
